d0cc101c6bf6c8cf33d5a7d01773eed983f0d581
Mayank Sharma <as.mayanksharma@gmail.com>
2022-05-30 17:42:08 +0530

    change the resolution and work on toolbar

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+from matplotlib.pyplot import draw
 import pygame
 pygame.init()
 
 fps = 60
 timer = pygame.time.Clock()
-WIDTH = 1600
-HEIGHT = 900
+WIDTH = 1300
+HEIGHT = 600
 
 screen = pygame.display.set_mode([WIDTH,HEIGHT])
 pygame.display.set_caption('Painting Tool')
+
+def draw_window():
+    pygame.draw.rect(screen,'gray',[0,0,70,HEIGHT])
 
 run = True
 while run:
     timer.tick(fps)
     screen.fill((255,255,255)) #white 
+    
+    draw_window()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
