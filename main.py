f123c8a09ed709da899c7cf47dac9e8147da691d
Mayank Sharma <as.mayanksharma@gmail.com>
2022-05-31 18:58:11 +0530

    change theme from Paint to Blackboard

--- main.py
+++ main.py
@@ -10,26 +10,26 @@ BRUSH_SIZE = 0
 DRAWING = []
 
 screen = pygame.display.set_mode([WIDTH,HEIGHT])
-pygame.display.set_caption('Painting Tool')
+pygame.display.set_caption('BlackBoard')
 
 def draw_window():
     pygame.draw.rect(screen,'gray',[0,0,50,HEIGHT]) #(x,y,w,h)  top left coordinates (x,y) width and height (w,h) 
-    pygame.draw.line(screen,'black',[50,0],[50,HEIGHT],2)
+    #pygame.draw.line(screen,'white',[50,0],[50,HEIGHT],2)
     brush = pygame.draw.rect(screen,'black',[10,10,30,30]) 
     brush_size = 7.5
     brush_circle = pygame.draw.circle(screen,'white',(25,25),7.5)
 
     return brush
-def draw():
+def drawing():
     for i in range(len(DRAWING)):
-        pygame.draw.circle(screen,'black',DRAWING[i][1],DRAWING[i][0]) # DRAWING[i][1] = the mouse value ; DRAWING[i][0] = the BRUSH_SIZE
+        pygame.draw.circle(screen,'white',DRAWING[i][1],DRAWING[i][0]) # DRAWING[i][1] = the mouse value ; DRAWING[i][0] = the BRUSH_SIZE
 
 
 
 run = True
 while run:
     timer.tick(fps)
-    screen.fill((255,255,255)) #white 
+    screen.fill('black')  
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()[0]
     if mouse[0] > 50:
@@ -45,7 +45,7 @@ while run:
                 BRUSH_SIZE = 7.5
     if click and mouse[0] > 70:
         DRAWING.append((BRUSH_SIZE,mouse))
-    draw()
+    drawing()
 
     
     pygame.display.flip()
