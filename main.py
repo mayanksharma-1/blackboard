4c274c5306c34fcd8493bea306ce455088226b87
Mayank Sharma <as.mayanksharma@gmail.com>
2022-06-04 00:08:09 +0530

    add chalk.svg as the button to pick up chalk

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 from matplotlib.pyplot import draw
-import pygame
+import pygame 
+import os
 pygame.init()
 
 fps = 120
 timer = pygame.time.Clock()
 WIDTH = 1300
 HEIGHT = 600
-BRUSH_SIZE = 0
+CHALK_SIZE = 0
 DRAWING = []
+CHALK_IMAGE = pygame.image.load(os.path.join("assets","chalk.svg"))
+CHALK_IMAGE = pygame.transform.scale(CHALK_IMAGE,(20,70))
 
 screen = pygame.display.set_mode([WIDTH,HEIGHT])
 pygame.display.set_caption('BlackBoard')
 
-def draw_window():
+def draw_window(CHALK_IMAGE):
     pygame.draw.rect(screen,'gray',[0,0,50,HEIGHT]) #(x,y,w,h)  top left coordinates (x,y) width and height (w,h) 
     #pygame.draw.line(screen,'white',[50,0],[50,HEIGHT],2)
-    brush = pygame.draw.rect(screen,'black',[10,10,30,30]) 
-    brush_size = 7.5
-    brush_circle = pygame.draw.circle(screen,'white',(25,25),7.5)
+    #chalk = pygame.draw.rect(screen,'black',[10,10,30,30]) 
+    chalk = screen.blit(CHALK_IMAGE,(10,10))
 
-    return brush
+    chalk_size = 7.5
+    #chalk_circle = pygame.draw.circle(screen,'white',(25,25),chalk_size)
+
+    
+
+    return chalk
 def drawing():
     for i in range(len(DRAWING)):
-        pygame.draw.circle(screen,'white',DRAWING[i][1],DRAWING[i][0]) # DRAWING[i][1] = the mouse value ; DRAWING[i][0] = the BRUSH_SIZE
+        pygame.draw.circle(screen,'white',DRAWING[i][1],DRAWING[i][0]) # DRAWING[i][1] = the mouse value ; DRAWING[i][0] = the CHALK_SIZE
 
 
 
@@ -33,18 +40,18 @@ while run:
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()[0]
     if mouse[0] > 50:
-        pygame.draw.circle(screen,'black',mouse,BRUSH_SIZE)
-    brush = draw_window()
+        pygame.draw.circle(screen,'white',mouse,CHALK_SIZE)
+    chalk = draw_window(CHALK_IMAGE)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if brush.collidepoint(event.pos):
-                BRUSH_SIZE = 7.5
+            if chalk.collidepoint(event.pos):
+                CHALK_SIZE = 7.5
     if click and mouse[0] > 70:
-        DRAWING.append((BRUSH_SIZE,mouse))
+        DRAWING.append((CHALK_SIZE,mouse))
     drawing()
 
     
